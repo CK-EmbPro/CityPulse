06981068d123ccbe7edf7b5f9fd93a7951fbab1d
CK-EmbPro <kennydebrice2@gmail.com>
2024-04-18 20:42:42 +0200

    p5 serialServer and client-side initialisation

---
+++ ImageModel_TM_2024_04_18_14_13_28/sketch.js
@@ -0,0 +1,76 @@
+// Classifier Variable
+let classifier;
+// Model URL
+let imageModelURL = './citypulse-model/';
+
+// Video
+let video;
+let flippedVideo;
+// To store the classification
+let label = "";
+
+// Serial port object
+let serial;
+
+// Load the model first
+function preload() {
+  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+}
+
+function setup() {
+  createCanvas(1920, 1080);
+  // Create the video
+  video = createCapture(VIDEO);
+  video.size(1920, 1080);
+  video.hide();
+
+  flippedVideo = ml5.flipImage(video);
+  // Start classifying
+  classifyVideo();
+
+  // Initialize serial port
+  serial = new p5.SerialPort();
+
+  // Set serial port parameters
+  serial.open("/dev/ttyACM0"); 
+  serial.on('open', () => {
+    console.log('Serial port opened');
+  });
+}
+
+function draw() {
+  background(0);
+  // Draw the video
+  image(flippedVideo, 0, 0);
+
+  // Draw the label
+  fill(255);
+  textSize(16);
+  textAlign(CENTER);
+  text(label, width / 2, height - 4);
+
+  // Send classification label to Arduino
+  serial.write(label);
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+  flippedVideo = ml5.flipImage(video)
+  classifier.classify(flippedVideo, gotResult);
+  flippedVideo.remove();
+
+}
+
+// When we get a result
+function gotResult(error, results) {
+  // If there is an error
+  if (error) {
+    console.error(error);
+    return;
+  }
+  // The results are in an array ordered by confidence.
+  // console.log(results[0]);
+  label = results[0].label;
+  // Classifiy again!
+  classifyVideo();
+}
